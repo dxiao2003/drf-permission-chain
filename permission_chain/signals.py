3c75689ff7c589248966028f61ed4ab84cef270f
David Xiao <dxiao@katapal.com>
2016-04-24 15:04:33 -0400

    add processor to signals

--- permission_chain/signals.py
+++ permission_chain/signals.py
@@ -4,11 +4,13 @@ from django.dispatch import Signal
 
 # to add additional chains, push onto the chain_generator array
 # a function that takes a request, view, obj as arguments
-get_additional_chains = Signal(providing_args=["chain_generator"])
+get_additional_chains = Signal(providing_args=["processor", "chain_generator"])
 
-get_additional_chain_fragments = Signal(providing_args=["fragments", "request",
-                                                        "view"])
+get_additional_chain_fragments = Signal(
+    providing_args=["processor", "fragments", "request", "view"]
+)
 
-process_additional_chain = Signal(providing_args=["chain", "result",
-                                                  "request", "view",
-                                                  "obj", "validated_data"])
+process_additional_chain = Signal(
+    providing_args=["processor", "chain", "result", "request", "view",
+                    "obj", "validated_data"]
+)
